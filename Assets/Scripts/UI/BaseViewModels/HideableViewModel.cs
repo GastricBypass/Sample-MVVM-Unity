d60ec228fcfe296ee77cf115a013303fbb8b659d
GastricBypass <dsp45@case.edu>
2026-02-02 09:54:00 -0800

    Added base MVVM implementation

---
+++ Assets/Scripts/UI/BaseViewModels/HideableViewModel.cs
@@ -0,0 +1,14 @@
+public abstract class HideableViewModel<T> : ViewModel<T> where T : Model
+{
+    public void SetVisible(bool visible)
+    {
+        gameObject.SetActive(visible);
+    }
+
+    protected override void OnModelChanged()
+    {
+        gameObject.SetActive(IsVisible());
+    }
+
+    protected abstract bool IsVisible();
+}
